6945598714bb0f578affa7086f9450febbf203c8
AkiraMatthew <akira.dev.math@gmail.com>
2023-12-27 17:48:20 -0300

    video bg scratch

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const path = require("path");
+const withVideos = require("next-videos");
+const nextConfig = {};
+
+module.exports = nextConfig;
 
 module.exports = {
     sassOptions: {
         includePaths: [path.join("sass", "src/app/css")],
     },
 };
+
+module.exports = withVideos();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./css/style.module.css";
 import LogoName from "../../public/imgs/logo-name.png";
+//import VideoBg from "../../public/video/akira-intro.mp4";
 import "./layout";
 
 export default function Home() {
@@ -10,58 +11,66 @@ export default function Home() {
                 <title>Akira Digital Solutions - Home</title>
             </head>
             <body className={`${styles.main}`} style={{ backgroundColor: "black" }}>
-                <header className={`${styles.container01} container-fluid pt-3`}>
-                    <div>
-                        <Image
-                            src={LogoName}
-                            alt="logo-name.png"
-                            style={{ width: "100px", height: "auto" }}
-                        />
-                    </div>
+                <div>
+                    <header className={`${styles.container01} container-fluid pt-3`}>
+                        <div>
+                            <Image
+                                src={LogoName}
+                                alt="logo-name.png"
+                                style={{ width: "100px", height: "auto" }}
+                            />
+                        </div>
 
-                    <div className={`${styles.containerFlex}`}>
-                        <a
-                            className={`px-1 ${styles.textColor}`}
-                            style={{
-                                textDecoration: "none",
-                            }}
-                        >
-                            TEAM
-                        </a>
-                        <a
-                            className={`px-1 ${styles.textColor2}`}
-                            style={{
-                                textDecoration: "none",
-                            }}
-                        >
-                            SERVICES
-                        </a>
-                        <a
-                            className={`px-1 ${styles.textColor}`}
-                            style={{
-                                textDecoration: "none",
-                            }}
-                        >
-                            ABOUT
-                        </a>
-                        <a
-                            className={`px-1 ${styles.textColor}`}
-                            style={{
-                                textDecoration: "none",
-                            }}
-                        >
-                            CONTACT
-                        </a>
-                        <a
-                            className={`px-1 ${styles.textColor}`}
-                            style={{
-                                textDecoration: "none",
-                            }}
-                        >
-                            EN | PT-BR
-                        </a>
-                    </div>
-                </header>
+                        <div className={`${styles.containerFlex}`}>
+                            <a
+                                className={`px-1 ${styles.textColor}`}
+                                style={{
+                                    textDecoration: "none",
+                                }}
+                            >
+                                TEAM
+                            </a>
+                            <a
+                                className={`px-1 ${styles.textColor2}`}
+                                style={{
+                                    textDecoration: "none",
+                                }}
+                            >
+                                SERVICES
+                            </a>
+                            <a
+                                className={`px-1 ${styles.textColor}`}
+                                style={{
+                                    textDecoration: "none",
+                                }}
+                            >
+                                ABOUT
+                            </a>
+                            <a
+                                className={`px-1 ${styles.textColor}`}
+                                style={{
+                                    textDecoration: "none",
+                                }}
+                            >
+                                CONTACT
+                            </a>
+                            <a
+                                className={`px-1 ${styles.textColor}`}
+                                style={{
+                                    textDecoration: "none",
+                                }}
+                            >
+                                EN | PT-BR
+                            </a>
+                        </div>
+                    </header>
+                    <video
+                        src={require("/public/video/akira-intro.mp4")}
+                        className={styles.video}
+                        muted
+                        autoPlay
+                    />
+                </div>
                 <footer></footer>
             </body>
         </html>
