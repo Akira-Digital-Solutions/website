3de9ac8268ea5b061f34956d30d0d602cffbe168
AkiraMatthew <akira.dev.math@gmail.com>
2024-01-04 14:35:35 -0300

    react-typewriter and new fonts added

--- components/textChange.tsx
+++ components/textChange.tsx
@@ -2,20 +2,22 @@
 import Typewriter from "react-typewriter-animate";
 import "react-typewriter-animate/dist/Typewriter.css";
 import React from "react";
+import styles from "../src/app/css/style.module.css";
 
 export class TypyingEffect1 extends React.Component {
     render() {
         return (
-            <div className="root">
+            <div className={`root ${styles.textColor2}`}>
                 <Typewriter
                     dataToRotate={[
                         // Banner 1
-                        [{ type: "word", text: "DEVELOP" }],
+                        [{ type: "word", text: " DEVELOP", cursor: { char: "_" } }],
                         // Banner 2
-                        [{ type: "word", text: "DESIGN" }],
-                        // Banner 2
-                        [{ type: "word", text: "BUILD" }],
+                        [{ type: "word", text: " DESIGN", cursor: { char: "_" } }],
+                        // Banner 3
+                        [{ type: "word", text: " BUILD", cursor: { char: "_" } }],
                     ]}
+                    loop
                 />
             </div>
         );
@@ -25,16 +27,41 @@ export class TypyingEffect1 extends React.Component {
 export class TypyingEffect2 extends React.Component {
     render() {
         return (
-            <div className="root">
+            <div className={`root ${styles.textColor2}`}>
                 <Typewriter
                     dataToRotate={[
                         // Banner 1
-                        [{ type: "word", text: "DEVELOP" }],
+                        [{ type: "word", text: " PROJECT", cursor: { char: "_" } }],
                         // Banner 2
-                        [{ type: "word", text: "DESIGN" }],
-                        // Banner 2
-                        [{ type: "word", text: "BUILD" }],
+                        [{ type: "word", text: " APP", cursor: { char: "_" } }],
+                        // Banner 3
+                        [{ type: "word", text: " DAPP", cursor: { char: "_" } }],
+                        // Banner 4
+                        [{ type: "word", text: " WEBSITE", cursor: { char: "_" } }],
+                        // Banner 5
+                        [{ type: "word", text: " BRAND", cursor: { char: "_" } }],
+                        // Banner 6
+                        [{ type: "word", text: " COMMUNITY", cursor: { char: "_" } }],
                     ]}
+                    loop
+                />
+            </div>
+        );
+    }
+}
+
+export class TypyingEffect3 extends React.Component {
+    render() {
+        return (
+            <div className={`root ${styles.textColor2}`}>
+                <Typewriter
+                    dataToRotate={[
+                        // Banner 1
+                        [{ type: "word", text: "WEB2", cursor: { char: "_" } }],
+                        // Banner 2
+                        [{ type: "word", text: "WEB3", cursor: { char: "_" } }],
+                    ]}
+                    loop
                 />
             </div>
         );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./css/style.module.css";
 import LogoName from "../../public/imgs/logo-name.png";
-import { TypyingEffect1, TypyingEffect2 } from "../../components/textChange";
+import { TypyingEffect1, TypyingEffect2, TypyingEffect3 } from "../../components/textChange";
 import "./layout";
 
 export default function Home() {
@@ -21,6 +21,7 @@ export default function Home() {
                     <header className={`${styles.container01} container-fluid p-3 px-5`}>
                         <div>
                             <Image
+                                id="logo"
                                 src={LogoName}
                                 alt="logo-name.png"
                                 style={{ width: "100px", height: "auto" }}
@@ -72,8 +73,18 @@ export default function Home() {
                     </header>
                     <div>
                         <div className={`${styles.videoContainer} container-fluid`}>
-                            <div>
-                                <TypyingEffect1 />
+                            <div className={styles.initialText}>
+                                <div className="d-flex flex-row">
+                                    <TypyingEffect1 />
+                                </div>
+                                <div className="d-flex flex-row">
+                                    <span style={{ fontSize: "25px" }}>BUILD YOUR ‎</span>
+                                    <TypyingEffect2 />
+                                </div>
+                                <div className="d-flex flex-row">
+                                    <span style={{}}>BRING YOUR ‎</span> <TypyingEffect3 /> BUSINESS
+                                    TO REALITY
+                                </div>
                             </div>
                             <video
                                 src={require("/public/video/videoBgV2.mp4")}
