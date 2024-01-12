82d31fe8214b3d0db53a6704a85ce1ac11a35c41
AkiraMatthew <akira.dev.math@gmail.com>
2024-01-12 16:44:44 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-/** @type {import('next').NextConfig} */
-
-// next.config.js
 const path = require("path");
 const withVideos = require("next-videos");
 
@@ -8,15 +5,4 @@ module.exports = withVideos({
     sassOptions: {
         includePaths: [path.join("sass", "src/app/css")],
     },
-    // Add your custom configurations here
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: "https://www.akiradigitalsolutions.com",
-    //             destination: "https://akiradigitalsolutions.com",
-    //             permanent: true, // 301 redirect
-    //         },
-    //         // Add more redirects as needed
-    //     ];
-    // },
 });
